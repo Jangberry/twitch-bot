5b0744b4de1daae2fe46ea38ed83eed8de217f5a
Mistercraft38 <matt2001@hotmail.fr>
2017-02-01 15:29:54 +0100

    Screen integration

--- Bot Twitch.py
+++ Bot Twitch.py
@@ -5,16 +5,18 @@ import time
 import threading
 import random
 from info import CHANNEL, PASS, NICK
+import lcd_i2c
 
 s = socket.socket()
 
 #Variables pour fonctions
+FichierQuotes = open("quotes.txt", "r")
 wiz = 0
 sel = -20
 grains = 0
 pause = False
 stop = False
-recurrenceMessages = ["Vous pouvez suivre @elemzje sur twitter: https://twitter.com/Elemzje", "Vous pouvez a tout moment ajouter le smurf d'@elemzje: lmjye. PC seulement, desolé les consoles :("]
+recurrenceMessages = ["Vous pouvez suivre @elemzje sur twitter: https://twitter.com/Elemzje", "Vous pouvez a tout moment ajouter le smurf d'@elemzje: lmjye sur Uplay."]
 
 def connection():  #Connection au serveur + channel
     print("connecting...")
@@ -44,9 +46,17 @@ def send(Message):   #Envoit de messages dans le Channel
         s.send("PRIVMSG " + CHANNEL + " :/me _ MrDestructoid : " + Message + "\r\n") #envoie message
         print("Envoyé : " + Message)
 
+lcd_i2c.main()
+lcd_i2c.Afficher("Connection...")
+log = open("crash.txt", "r")
+print(log.read())
+log.close
 connection()
 send("/me Le bot de l'enfer est de retour, cachez vous !!!")
 threading.Thread(target=recurrence).start()
+log = open("crash.txt", "w")
+log.write("Wipe au restart, le : "+time.strftime("%c"))
+log.close
 
 try:
  while 1:
@@ -67,8 +77,32 @@ try:
 
             ###______Commandes______###
 
-    if user == "lawry25":
-        send("Kappa")
+
+    if "!quote" in text:
+        if len(text.split(" "))>0:
+            quote = text.split(" ")[-1]
+        else:
+            quote = text.split("quote")[1]
+        if "s" in quote:
+            pass
+        else:
+            quote = quote.split("\r")[0]
+            if "quote" in quote:
+             send("veuillez indiquer quelle quote vous voullez")
+             pass
+            else:
+             quote = str(int(quote))
+             quotes = FichierQuotes.read()
+             quotes = quotes.split("#~"+quote)[0]
+             quote = quotes.split("~#")[-1]
+             send(quote)
+
+    if user == "lawry25" and lawry == True:
+    	send(Kappa)
+    	if "stop kappa" in text:
+    		lawry = False
+    if "reKappa" in text:
+    	lawry = True
 
     if " c " in text and not ("ctrl" in text or "ctl" in text or "contr" in text) and sel < 20:
         if sel < 1:
@@ -87,6 +121,12 @@ try:
         	send("On n'écrit pas \"t\" quand on parle francais... on écrit \"t'es\", \"thé\" ou \"tes\" @"+user)
         else:
             send("*\"t'es\" ou \"tes\" @"+user)
+
+    if " g " in text and sel < 20:
+        if sel < 1:
+            send("On dis pas \"g\" quand on parle francais, on ecrit \"j'ai\" @"+user)
+        else:
+            send("*j'ai @" + user)
         
     if ("blg" or "BLG" or "beluga" or "Beluga" or "béluga" or "Béluga") in text:
         send("sckBLG sckBLG sckBLG")
@@ -131,9 +171,9 @@ try:
     
     if "!pseudo" in text and len(text.split(" ")) < 2:
         send("il était une fois, dans une lointaine contrée naz.. eu non.. il  était une fois, en alsace, un jeune CM1 prénomé Bryan (brillant... LOL). Lors d'une journée d'orage, il jouait avec ses amis. Cependant, avec ses amis, il jouais au foot. L'orage n'etait pas habituel (ciel violet, pluie fine et tout le tralala). ...")
-        send("... Avec ses amis, ils s'amusaient à dire \"les elements se dechainent, les elements se déchainent\", ensuite, en classe, ils continuaient avec les elements, leur maîtresse dit \"oui bien l'element, il vas se calmer\". Depuis, element,est resté et s'est transformé en @elemzje. \"zje\" étant là uniquement \"pour faire chier les gens\".")
+        send("... Avec ses amis, ils s'amusaient à dire \"les elements se dechainent, les elements se déchainent\", ensuite, en classe, ils continuaient avec les elements, leur maîtresse dit \"oui bien l'element, il vas se calmer\". Depuis, element,est resté et s'est transformé en @elemzje. \"zje\" étant là uniquement, je cite, \"pour faire chier les gens\".")
 
-    if ("!sale" or "!salé") in text:
+    if ("!saler" or "!sale" or "!salé") in text:
         sel = sel+50
         grains = grains+50
         send("Le niveau de PJSalt est reglé à " + str(sel))
@@ -143,7 +183,7 @@ try:
     if "!sel" in text:
         send("Le niveau de PJSalt actuel est de " + str(sel))
 
-    if ("!sucre" or "!sucré") in text:
+    if ("!sucre" or "!sucré" or "!sucrer") in text:
         sel = sel-50
         grains = grains+50
         send("Le niveau de PJSalt est reglé à "+ str(sel))
@@ -172,8 +212,26 @@ try:
         if grains > 1000:
         	send("c'est le grain de sucre de @" + user + " qui fait déborder le vase... sel reinitialisé à -20")
         	sel = -20
+        	
+    if "!refresh" in text.split(" ")[0]:
+    	FichierQuotes.close
+    	FichierQuotes = open("quotes.txt", "r")
 
-
+    if "!addquote" in text.split(" ")[0] and len(text.split(" ")) > 2:
+        quote = ""
+        for i in range(1, len(text.split(" "))-1):
+            quote = quote+str(text.split(" ")[i])+" "
+        last = FichierQuotes.read()
+        last = last.split("#~")[-1]
+        last = int(last.split("\n")[0])
+        FichierQuotes.close
+        FichierQuotes = open("quotes.txt", "a")
+        quote = quote.split("\r\n")[0]
+        FichierQuotes.write("~#"+quote+"#~"+str(last+1)+"\n")
+        FichierQuotes.close
+        send("Quote enregistrée comme quote n°"+str(last+1))
+        print("New quote = "+quote)
+        FichierQuotes = open("quotes.txt", "r")
 
 
 except KeyboardInterrupt:
@@ -182,6 +240,15 @@ except KeyboardInterrupt:
     send("Votre bot bot préféré s'en vas sur demande imperative de son maitre suprême... Le bot reviendra potentiellement bientôt ;) sckHLT")
     send("/disconnect")
     print("En attente de la fin du thread recurrence...")
-    for i in range(0, 5):
-        print("...")
-        time.sleep(1)
+    exit()
+
+except Exception, e:
+    print(str(e))
+    log = open("crash.txt", "a")
+    log.write(time.strftime("%c")+" : "+e)
+    log.close
+    send("Ce robot a crash... Merci d'en informer son créateur... J'AI ENVIE D'ETRE UN BOT SANS BUG !!! Mais je reste quand même, ne vous inquietez pas ;) ")
+    pass
+
+finally:
+    lcd_byte(0x01, LCD_CMD)
