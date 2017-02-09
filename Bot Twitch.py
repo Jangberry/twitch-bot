20a4217266ac94bd3b2561615f27f1eb610451c3
Mistercraft38 <matt2001@hotmail.fr>
2017-02-09 12:57:19 +0100

    Add files via upload

--- Bot Twitch.py
+++ Bot Twitch.py
@@ -1,22 +1,21 @@
 #-*- coding: utf-8 -*-
-import socket
-import sys
-import time
-import threading
-import random
+import socket, sys, time, threading, random, urllib, json, lcd_i2c
 from info import CHANNEL, PASS, NICK
-import lcd_i2c
 
 s = socket.socket()
 
 #Variables pour fonctions
 FichierQuotes = open("quotes.txt", "r")
+user = ''
 wiz = 0
 sel = -20
 grains = 0
 pause = False
 stop = False
-recurrenceMessages = ["Vous pouvez suivre @elemzje sur twitter: https://twitter.com/Elemzje", "Vous pouvez a tout moment ajouter le smurf d'@elemzje: lmjye sur Uplay."]
+chatnb = 0
+users = []
+modos = []
+recurrenceMessages = ["Vous pouvez suivre @elemzje sur twitter: https://twitter.com/Elemzje", "Vous pouvez a tout moment ajouter le smurf d'@elemzje : lmjye sur Uplay."]
 
 def connection():  #Connection au serveur + channel
     lcd_i2c.Afficher("Connexion...")
@@ -32,17 +31,53 @@ def connection():  #Connection au serveur + channel
 
 def log(LOG):
     logfile.write(time.ctime() + " $ " + LOG + " \r\n")
-    
+
+def channelInfo():
+    global users
+    global modos
+    global chatnb
+    infos = urllib.urlopen("https://tmi.twitch.tv/group/user/"+ CHANNEL.split("#")[1] +"/chatters")
+    infos = json.loads(infos.read())
+    users = infos["chatters"]["viewers"]
+    #print(str(users))
+    modos = infos["chatters"]["moderators"] + infos["chatters"]["global_mods"] + infos["chatters"]["staff"] + infos["chatters"]["admins"]
+    #print(str(modos))
+    chatnb = infos["chatter_count"]
+    #print(str(chatnb))
+    #print(user in modos)
+
+def newchat():
+    global chatnb
+    while stop == 0:
+        while pause == 0:
+            for i in range(0, 300, 5):
+                time.sleep(5)
+        if chatnb != chatlt:
+            if chatnb > chatl:
+                send("["+str(chatnb)+" viewers (+"+str(chatlt-chatnb)+")]")
+                send("Bienvenue au(x) nouvel/nouveaux arrivant(s), sachez que ce compte est à la fois un bot, et un modo humain (en chere et en circuits imprimés... oui oui... les deux) et certaint peuvent trouver le bot irritant... veuillez m'en escuser.")
+            elif chatnb < chatlt:
+                send("["+str(chatnb)+" viewers ("+str(chatlt-chatnb)+")]")
+        chatlt = chatnb
+
 def recurrence():
  try:
     while stop == 0:
         time.sleep(1)
+        messages = 0
         while pause == 0 and stop == 0:
-            send(recurrenceMessages[random.randint(0, len(recurrenceMessages)-1)])
-            for i in range(0, 1000, 5):
-                time.sleep(5)
+            send(recurrenceMessages[messages])
+            messages= messages+1
+            if messages == len(recurrenceMessages):
+                messages=0
+            for i in range(0, 1300, 10):
                 if pause == 1 or stop == 1:
                     break
+                time.sleep(5)
+                channelInfo()
+                if pause == 1 or stop == 1:
+                    break
+                time.sleep(5)
     exit()
  except Exception, e:
     print(str(e))
@@ -55,7 +90,7 @@ def send(Message):   #Envoit de messages dans le Channel
         print("Commande : " + Message)
     else:
         s.send("PRIVMSG " + CHANNEL + " :/me _ MrDestructoid : " + Message + "\r\n") #envoie message
-        print("Envoyé : " + Message)
+        print("Envoyé : " +Message)
 
 lcd_i2c.main()
 logfile = open("log.txt", "a")
@@ -66,29 +101,36 @@ crashlog.close
 connection()
 send("/me Le bot de l'enfer est de retour, cachez vous !!!")
 threading.Thread(target=recurrence).start()
+threading.Thread(target=newchat).start()
 crashlog = open("crash.txt", "w")
 crashlog.write("Wipe au restart, le : "+time.strftime("%c"))
 crashlog.close
+channelInfo()
 
 try:
  while 1:
 
+    #print(user+str(modos))
+    #print(user in modos)
     text = ""
     user = ""
     recu = s.recv(2040)
+    log(recu)
     if len(recu.split(":")) >= 3:      #séparation user/texte
         user = recu.split("!")[0]
         user = user.split(":")[1]
         for i in range(2, len(recu.split(":")), 1):
             text = text + recu.split(":")[i] + ":"
         print(user+" : "+text)      #log
-        log(user + " : " + text)
+
     elif "PING" in recu:        #pong
         rep = recu.split(":")[1]
         s.send("PONG :" + rep + "\r\n")
         print("Ping")
         stop = False
         pause = False
+    elif "RECONNECT" in text:
+        connection()
 
             ###______Commandes______###
 
@@ -113,28 +155,34 @@ try:
              send(quote)
 
     if user == "lawry25" and lawry == True:
-    	send(Kappa)
+        send(Kappa)
         lcd_i2c.Afficher("Kappa  " + str(sel), "Vive mistercraft")
-    	if "stop kappa" in text:
-    		lawry = False
+        if "stop kappa" in text:
+            lawry = False
     if "reKappa" in text:
-    	lawry = True
+        lawry = True
+
+    if "salut" in text and "@mistercraft38" in text:
+        send("sckHLT ations camarade !")
+
+    if ((" vas " in text or " vas-" in text) and "comment" in text) and "@mistercraft38" in text:
+        send("Je vais tres bien, merci... mais c'est de la triche: je suis un bot...")
 
     if " c " in text and not ("ctrl" in text or "ctl" in text or "contr" in text) and sel < 20:
         if sel < 1:
-        	send("On n'écrit pas \"c\" quand on parle français... on écrit \"c\'est\", \"ces\", \"ses\" ou encore \"sait\" @"+user)
+            send("On n'écrit pas \"c\" quand on parle français... on écrit \"c\'est\", \"ces\", \"ses\" ou encore \"sait\" @"+user)
         else:
             send("*\"c'est\" ou \"ces\" @" + user)
 
     if " pa " in text and sel < 20:
-    	if sel < 1:
-    		send("On met un \"s\" à la fin de \"pas\" @"+user)
-    	else:
-    		send("*pas @" + user)
+        if sel < 1:
+            send("On met un \"s\" à la fin de \"pas\" @"+user)
+        else:
+            send("*pas @" + user)
 
     if " t " in text and sel <20:
         if sel < 1:
-        	send("On n'écrit pas \"t\" quand on parle francais... on écrit \"t'es\", \"thé\" ou \"tes\" @"+user)
+            send("On n'écrit pas \"t\" quand on parle francais... on écrit \"t'es\", \"thé\" ou \"tes\" @"+user)
         else:
             send("*\"t'es\" ou \"tes\" @"+user)
 
@@ -143,6 +191,33 @@ try:
             send("On dis pas \"g\" quand on parle francais, on ecrit \"j'ai\" @"+user)
         else:
             send("*j'ai @" + user)
+
+    if " etai " in text and sel <20:
+        if sel < 1:
+            send("À l'imparfait, le verbe \"être\" se conjugue \"étais\" à la premiere et à la seconde personne du singulier et \"etait\" à la troisieme personne. Apres si tu voulais mettre une personne du pluriel, t'es mal barré, car meme en phonetique ca ne s'ecrit pas comme ca... Sinon, son participe passé s'ecrit \"été\", comme la saison, mais je ne viendrai pas te faire chier si tu oublie les accents ;) @"+user)
+        else:
+            send("*étais ou était @"+user)
+
+    if " bi1 " in text and sel <20:
+        if sel < 1:
+            send("Serieux ? Le langage sms c'est pour les claviers T9... pas azerty, et sauf information contraire, tu ne regarde pas le stream sur un 3310 @"+user)
+        else:
+            send("*bien @"+user)
+
+    if " tro " in text and sel <20:
+        if sel < 1:
+            send("Il y a un \"p\" à la fin de \"trop\". @"+user)
+        else:
+            send("*trop @"+user)
+
+    if " g etai " in text:
+        send("BON DIEU ! Rassure-moi, tu fait expres ? @"+user+" ca me ferai mal au cœur de savoir que quelqu'un aie une telle ignorance de l'existance du beschrelle")
+
+    if " ct " in text and sel <20:
+        if sel < 1:
+            send("Mouaif... j'ai vus mieux en terme d'orthographe... tu sais ce n'est pas si long de taper \"c'était\" en toutes lettre, ca t'entraines meme à la dyctalographie... tres utile de nos temps, crois-moi @"+user)
+        else:
+            send("*c'était @"+user)
         
     if ("blg" or "BLG" or "beluga" or "Beluga" or "béluga" or "Béluga") in text:
         send("sckBLG sckBLG sckBLG")
@@ -150,8 +225,9 @@ try:
     if ("HLT" in text or "salut" in text) and user != "mistercraft38":
         send("sckHLT @" + user)
         
-    if "!to " in text and user == "mistercraft38":
-        if "!to" in text.split(" ")[0] and len(text.split(" ")) == 3:
+    if "!to " in text and user in modos:
+        print('to')
+        if "!to" in text.split(" ")[0] and len(text.split(" ")) > 2:
             send("/timeout " + text.split(" ")[1] + " " + text.split(" ")[2])
 
     if user == "wizebot" and wiz == 0:
@@ -187,7 +263,7 @@ try:
         send("MONITOR : BenQ XL2411Z (144 Hz ! OMG!!!), HEADSET : HYPER X CLOUD II, MOUSE : STEELSERIES Rival (la 1ere), MOUSEPAD : STEELSERIES Qck Heavy (lol j'ai le même Kappa ), KEYBOARD : STEELSERIES APEX M800, MB : ASUS H81-PLUS, CPU : INTEL i5-4690, GPU : MSI GTX 970 4GB (j'ai autant de vram mais bon... j'ai une 750Ti...), HDD : SEAGATE Barracuda 1 To, SDD : SEAGATE 250 Go (riche...), RAM : 2 x 4 Go CORSAIR Vengeance, PSU : CORSAIR 550W.")
     
     if "!pseudo" in text and len(text.split(" ")) < 2:
-        send("il était une fois, dans une lointaine contrée naz.. eu non.. il  était une fois, en alsace, un jeune CM1 prénomé Bryan (brillant... LOL). Lors d'une journée d'orage, il jouait avec ses amis. Cependant, avec ses amis, il jouais au foot. L'orage n'etait pas habituel (ciel violet, pluie fine et tout le tralala). ...")
+        send("il était une fois, dans une lointaine contrée naz.. eu non.. il  était une fois, en alsace, un jeune CM1 prénomé Bryan (brillant... LOL). Lors d'une journée d'orage, il jouait avec ses amis. Il jouais au foot. L'orage n'etait pas habituel (ciel violet, pluie fine et tout le tralala). ...")
         send("... Avec ses amis, ils s'amusaient à dire \"les elements se dechainent, les elements se déchainent\", ensuite, en classe, ils continuaient avec les elements, leur maîtresse dit \"oui bien l'element, il vas se calmer\". Depuis, element,est resté et s'est transformé en @elemzje. \"zje\" étant là uniquement, je cite, \"pour faire chier les gens\".")
 
     if ("!saler" or "!sale" or "!salé") in text:
@@ -195,7 +271,7 @@ try:
         grains = grains+50
         send("Le niveau de PJSalt est reglé à " + str(sel))
         if grains > 1000:
-        	grains = grains - 50
+            grains = grains - 50
         lcd_i2c.AfficherLine("Sel: "+str(sel), "Vive mistercraft")
 
     if "!sel" in text:
@@ -207,23 +283,23 @@ try:
         grains = grains+50
         send("Le niveau de PJSalt est reglé à "+ str(sel))
         if grains > 1000:
-            	grains = grains - 50
+                grains = grains - 50
         lcd_i2c.AfficherLine("Sel: "+str(sel), "Vive mistercraft")
 
     if (" con " or " merde " or " chiant ") in text:
         sel = sel + 1
         if "Kappa" in text or "<3" in text:
-        	sel = sel-6
-        	grains = grains-5
+            sel = sel-6
+            grains = grains-5
         elif "mistercraft" in text:
-        	sel = sel + 10
+            sel = sel + 10
         grains = grains +1
         print(str(sel))
         if grains > 1000:
-        	send("c'est le grain de sel de @" + user + " qui fait déborder le vase... sel reinitialisé à 20")
-        	sel = 20
+            send("c'est le grain de sel de @" + user + " qui fait déborder le vase... sel reinitialisé à 20")
+            sel = 20
         lcd_i2c.AfficherLine("Sel: "+str(sel), "Vive mistercraft")
-		
+        
     if (" amour " or " aime " or "<3" or "Kappa") in text:
         sel = sel - len(text.split("Kappa"))
         sel = sel - len(text.split("<3"))
@@ -231,13 +307,13 @@ try:
         print(str(sel))
         grains = grains + 2
         if grains > 1000:
-        	send("c'est le grain de sucre de @" + user + " qui fait déborder le vase... sel reinitialisé à -20")
-        	sel = -20
+            send("c'est le grain de sucre de @" + user + " qui fait déborder le vase... sel reinitialisé à -20")
+            sel = -20
         lcd_i2c.AfficherLine("Sel: "+str(sel), "Vive mistercraft")
-        	
+            
     if "!refresh" in text.split(" ")[0]:
-    	FichierQuotes.close
-    	FichierQuotes = open("quotes.txt", "r")
+        FichierQuotes.close
+        FichierQuotes = open("quotes.txt", "r")
 
     if "!addquote" in text.split(" ")[0] and len(text.split(" ")) > 2:
         quote = ""
@@ -257,6 +333,18 @@ try:
         lcd_i2c.AfficherLine("New quote:"+quote[:18], "...")
         FichierQuotes = open("quotes.txt", "r")
 
+    if "!tauhazard" in text:
+      if len(users) == 0:
+        pass
+      else:
+        channelInfo()
+        send("Tirage au sort d'un personne à timeout parmis les "+str(chatnb)+" personnes presentes dans le chat...")
+        to = users[random.randint(0, len(users)-1)]
+        send(to + " a été tiré au sort pour un to de 100 secondes. Un dernier mot ? tu as 10 secondes...")
+        time.sleep(10)
+        send("/timeout "+to+" 100")
+        send("Au plaisir @"+to)
+    
     if "!demo" in text:
         lcd_i2c.AfficherLine("Vive mistercraft", "presque Kappa")
 
@@ -265,7 +353,7 @@ except KeyboardInterrupt:
     pause = True
     send("Votre bot bot préféré s'en vas sur demande imperative de son maitre suprême... Le bot reviendra potentiellement bientôt ;) sckHLT")
     send("/disconnect")
-    print("En attente de la fin du thread recurrence...")
+    print("En attente de la fin des threads...")
     log("Extinction du Bot: KeyboardInterrupt \r\n")
     lcd_i2c.Afficher("KeyboardInterrupt", "Fin")
 
@@ -275,8 +363,8 @@ except Exception, e:
     crashlog = open("crash.txt", "a")
     crashlog.write(time.strftime("%c")+" : "+str(e))
     crashlog.close
-    log(e)
-    send("Ce robot a crash... Merci d'en informer son créateur... J'AI ENVIE D'ETRE UN BOT SANS BUG !!!")
+    log(str(e))
+    send("Ce robot a crash... Merci d'en informer son créateur... J'AI ENVIE D'ETRE UN BOT SANS BUG !!! Erreur :"+str(e))
     lcd_i2c.Afficher("Bug:"+str(e))
     stop = True
     pause = True
@@ -284,6 +372,6 @@ except Exception, e:
 
 finally:
     log("Fin de l'execution/fin du log \r\n \n")
-    log.close
+    logfile.close
     time.sleep(5)
     lcd_i2c.close()
